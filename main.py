708caf37da365c425e236b7d3013073a6c0791a9
Halimi Lior <lior.halimi@yahoo.fr>
2026-05-20 01:47:58 +0300

    adding pygame and creating a screen

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello, World!")
+
+import pygame
+
+
+pygame.init()
+
+WIDTH = 600
+HEIGHT = 600
+CELL_SIZE = 20
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Snake Game")
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+
+    pygame.display.update()
+    clock.tick(10)
+
+pygame.quit()
---
+++ the_snake.py
@@ -0,0 +1,4 @@
+class snake:
+    def __init__(self):
+        self.body = [(0, 0)]
+        self.direction = (1, 0)  # Initial direction: right
