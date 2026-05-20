f1c07ad6fbc3cb8f0a6e34df52da7365952c733b
Halimi Lior <lior.halimi@yahoo.fr>
2026-05-20 18:17:10 +0300

    adding size choice

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 import pygame
 class Settings:
-    def __init__(self, Width, Height, fps=7):
-        self.grid_width = Width // 20
-        self.grid_height = Height // 20
-        self.cell_size = 20
+    def __init__(self, Width, Height, fps=7, cell_size=20, snake_size=None):
+        self.grid_width = Width // cell_size
+        self.grid_height = Height // cell_size
+        self.cell_size = cell_size
         self.fps = fps
         self.width = Width
         self.height = Height
